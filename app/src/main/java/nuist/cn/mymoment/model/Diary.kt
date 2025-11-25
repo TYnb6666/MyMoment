414fdad1cfbad3773154591fb9fa7573a964d0be
WangZixu <202283910017@nuist.edu.cn>
2025-11-25 15:41:11 +0800

    Test branch functions

--- app/src/main/java/nuist/cn/mymoment/model/Diary.kt
+++
@@ -1,9 +0,0 @@
-package nuist.cn.mymoment.model
-
-// Define the data structure of diary entries
-data class Diary(
-    val id: String = "",
-    val title: String = "",
-    val content: String = "",
-    val date: Long= System.currentTimeMillis()
-)
